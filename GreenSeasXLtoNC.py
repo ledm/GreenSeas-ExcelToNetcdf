073124c748cc868b7ca0acbd3edb7adbc77f3175
Lee de Mora <ledm@pml.ac.uk>
2013-04-19 17:06:43 +0100

    new changes, it's functional, but lacking in functionality. end of week commit with working code.

--- GreenSeasXLtoNC.py
+++ GreenSeasXLtoNC.py
@@ -1,26 +1,23 @@
 from mmap import mmap,ACCESS_READ
-from xlrd import open_workbook,cellname
+from xlrd import open_workbook,cellname,empty_cell
 from os.path import exists
 from os import makedirs
 from shelve import open as shOpen
-from netCDF4 import Dataset
+try:from netCDF4 import Dataset,default_fillvals
+except: from netCDF4 import Dataset, _default_fillvals
 from datetime import date,datetime
 from getpass import getuser
-from numpy import array
+from numpy import array, float64, int64,int32
 from numpy.ma import array as marray, masked_where
 
-#try:	, default_fillvals
-#except: from netCDF4 import Dataset, _default_fillvals
+# Things to do:
+	#correct logging
+	#sort out date/time
+	#extend beyond temperature
+	#include more attributes.
+	#include data quality.
+	#clearer comments.
 
-#import logging
-
-
-# add metadata, and columns that are all the same value to netcdf attributes
-# turn it into a class
-
-# plans take command line file name and variable
-
-# push it to git hub
 
 class GreenSeasXLtoNC:
   def __init__(self, fni, fno, debug=True,saveShelve=False,saveNC=True):
@@ -75,27 +72,12 @@ class GreenSeasXLtoNC:
 	units    = [h.value for h in self.datasheet.row(2)]
 	locator  = [h.value for h in self.datasheet.row(11)[0:20]]
 	metadata = [h.value for h in self.datasheet.col(10)[0:12]]
-
+	time     = [h.value for h in self.datasheet.col(9)[20:]]
+	
 	print 'GreenSeasXLtoNC:\tInfo:\tlocators:',locator
-	
-	key ={}
-	for n,l in enumerate(locator):
-		if l.lower() in [ 'lat', 'latitude']: key['lat']=n
-		if l.lower() in [ 'lon','long', 'longitude']: key['lon']=n
-		if l in [ 'time','t', 'Date& Time (local)']: key['time']=n
-		if l in [ 'Depth of sample [m]']: key['z']=n
-		if l in [ 'Depth of Sea [m]',]: key['bathy']=n
-		if l in [ 'UTC offset',]: key['tOffset']=n
-		if l in ['Institute',]: key['Institute']=n
 
-	# create location t,z,y,x data
-	#lat  = [h.value for h in self.datasheet.col(key['lat'])[20:]]
-	#lon  = [h.value for h in self.datasheet.col(key['lon'])[20:]]
-	#time = [h.value for h in self.datasheet.col(key['time'])[20:]]
-	#depth= [h.value for h in self.datasheet.col(key['z'])[20:]]
 	
-	
-	# add location and DQ info to netcdf
+	# add location to netcdf
 	saveCols=[]
 	lineTitles={}
 	unitTitles={}
@@ -107,7 +89,8 @@ class GreenSeasXLtoNC:
 		unitTitles[l]=''
 		if loc.find('[') > 0:
 		  unitTitles[l]=loc[loc.find('['):]
-	# add data columns to output to netcdf.
+		  
+	# add data columns titles to output to netcdf.
 	for h,head in enumerate(header):
 		if head == '':continue	
 		for d in self.datanames: 
@@ -116,7 +99,6 @@ class GreenSeasXLtoNC:
 				saveCols.append(h)
 				lineTitles[h] = header[h]
 				unitTitles[h] = units[h]				
-
 	saveCols = sorted(saveCols)	
 	
 	print 'GreenSeasXLtoNC:\tInfo:\tSaving data from columns:',saveCols
@@ -126,44 +108,69 @@ class GreenSeasXLtoNC:
 	data={}
 	for d in saveCols:
 		data[d]= [h.value for h in self.datasheet.col(d)[20:]]
-		data[d] = masked_where(data[d]=='', data[d])
 		
+		#if d<20: data[d] =  masked_where(data[d]=='', data[d]) # this doesn't work elsewhere, as sets all to strings, when one string is present, even if its empty.
+		#else:
+		arr = []
+		isaString = self._isaString_(lineTitles[d])
+		for a in data[d][:]:
+			if isaString:
+				if a == empty_cell:arr.append(default_fillvals['S1'])
+				else: arr.append(str(a))
+			else:
+				if a == empty_cell:
+				    arr.append(default_fillvals['f4'])
+				else:
+			  	    try:   arr.append(float(a))
+			  	    except:arr.append(default_fillvals['f4'])
+		arr = marray(arr)
+		print len(arr), arr.dtype
+		data[d] = arr
+				
+				
 
-	# count number of entries in each column:
-	datacounts = {h: 0 for h in saveCols}
-	for i in xrange(len(lat)):
-		a = [data[d][i] for d in saveCols]
-		if a.count('') == len(a):continue
-		#print '\n\n',count,i,':\tt,z,y,x:',time[i].value,depth[i].value,lat[i].value,lon[i].value,
-		#print 'data:',
-		for d in saveCols: 
-			if data[d][i]:
-			 	datacounts[d]+=1
-			#print header[d],data[d][i]
+	
+	# count number of datapoints in each column:
+	datacounts = {d:0 for d in saveCols}
+	for d in saveCols:
+		for i in data[d][:]:
+	 		if i: datacounts[d]+=1
 	print 'GreenSeasXLtoNC:\tInfo:\tNumber of entries in each datacolumn:', datacounts
 	
-	# look for data columns with no data
+	
+	# count number of datapoints in each row:
+	rowcounts = {d:0 for d in xrange(len(time))}
+	for r in sorted(rowcounts.keys()):
+		for d in saveCols:
+			if d<20:continue
+			#if rowcounts[r]: break
+			if data[d][r] in ['', None, default_fillvals['f4'],]: continue
+			rowcounts[r] += 1
+			#print r,d,data[d][r],rowcounts[r] 
+			
+	print 'GreenSeasXLtoNC:\tInfo:\tNumber of entries in each rowcounts:', rowcounts
+	
+	
+	# list data columns with no data
 	emptyColummns=[]
 	for h in saveCols:
 		if datacounts[h] == 0:
-			print 'GreenSeasXLtoNC:\tInfo:\tNo data for coumn ',h,lineTitles[h],'[',unitTitles[h],']'
+			print 'GreenSeasXLtoNC:\tInfo:\tNo data for column ',h,lineTitles[h],'[',unitTitles[h],']'
 			emptyColummns.append(h)
 	print 'GreenSeasXLtoNC:\tInfo:\tEmpty Columns of "data":', emptyColummns
 		
-	# look for data columns with only one value	
+	# list data columns with only one value	
 	oneValueInColumn=[]
+	attributes={}
 	for h in saveCols:
 		if h in emptyColummns:continue
 		col = sorted(data[h])
-		#print h,lineTitles[h],'[',unitTitles[h],']','col:', col[0], col[-1]
 		if col[0] == col[-1]:
 			print 'GreenSeasXLtoNC:\tInfo:\tonly one "data": ',lineTitles[h],'[',unitTitles[h],']','value:', col[0]
 			oneValueInColumn.append(h)
-			continue
-		
+			attributes[lineTitles[h]] = col[0]
 	print 'GreenSeasXLtoNC:\tInfo:\tColumns with only one "data":', oneValueInColumn
-	
-	
+	print 'GreenSeasXLtoNC:\tInfo:\tnew file attributes:', attributes	
 	
 	# Meta data for those columns:
 	ncVarName={}
@@ -179,34 +186,42 @@ class GreenSeasXLtoNC:
 		
 
 	# figure out data type:
+	# netcdf4 requries some strange names for datatypes: 
+	#	ie f8 instead of numpy.float64
 	dataTypes={}
 	for h in saveCols:
 		if h in emptyColummns:continue
 		if h in oneValueInColumn:continue
-		col = sorted(data[h])
-		if type(col[0]) == type(col[-1]):
-			dataTypes[h] = type(col[0])
-			
-			print 'GreenSeasXLtoNC:\tInfo:\t',h, ncVarName[h], ' is type:',  dataTypes[h]
-		else:
-			print 'GreenSeasXLtoNC:\tWARNING:\tTWO KINDS OF DATA IN', h, ncVarName[h], [col[0],type(col[0]) ],[col[-1], type(col[-1])]
+		dataTypes[h] = data[h].dtype
+		print h,dataTypes[h],
+		if dataTypes[h] == float64: dataTypes[h] = 'f8'
+		elif dataTypes[h] == int32: dataTypes[h] = 'i4'
+		elif dataTypes[h] == int64: dataTypes[h] = 'i8'	
+		#elif dataTypes[h] =='|S1':
+		#	dataTypes[h] = 'f8'
+		#	#data[h] = float64(data[h])
+		else: dataTypes[h] = 'S1'
+		print dataTypes[h]
 		
 		
 			
 	self.saveCols = saveCols
 	self.emptyColummns = emptyColummns
 	self.oneValueInColumn = oneValueInColumn
+	self.rowcounts=rowcounts
 	self.ncVarName = ncVarName
 	self.dataTypes = dataTypes
 	self.data = data
 	self.lineTitles = lineTitles
 	self.unitTitles = unitTitles
+	self.attributes = attributes
 	#self.lat = lat
 	#self.lon = lon
 	#self.time = time
 	#self.depth = depth
 
-  def _getNCvarName_(self,locName): 	
+  def _getNCvarName_(self,locName): 
+ 	# users are welcome to expand this list to include other elements of the green seas database.
   	exceptions = {	'Depth of Sea [m]': 'Bathymetry', 
 		'Depth of sample [m]': 'Depth', 
 		'Date& Time (local)': 'Time', 
@@ -218,14 +233,30 @@ class GreenSeasXLtoNC:
 		'GS Originator / PI': 'gsOriginator', 
 		'Originator / PI': 'originator', 
 		'Research Group(s) if relevant':'researchGroup',}
-	if locName in exceptions:
-		#print 'GreenSeasXLtoNC:\tInfo:\tgetNCvarName ', locName,'->', exceptions[locName]
-		return exceptions[locName]
-	else:
-		#print 'GreenSeasXLtoNC:\tInfo:\tgetNCvarName ', locName,'->', locName.replace(' ','')
-		return locName.replace(' ','')
+	if locName in exceptions: return exceptions[locName]
+	else: return locName.replace(' ','')
 		
 	
+  def _isaString_(self,locName): 
+	if locName in [ 'Date& Time (local)', 'Explanation/ reference of any conversion factors or aggregation used (if relevant)',
+			'measure type1', 'measure type2', u'duplicated (1=Y, 0=N)', u'GS Originator / PI', u'Originator / PI',
+			 u'Institute', u'Research Group(s) if relevant']: return True
+	return False
+
+
+	
+  #def _getDataTypes_(self,col):
+  	#scol = sorted(col)
+  	#if type(max(scol)) == type(min(scol)):
+  	#	t = type(col[0])
+  	#	
+  	#if type(col[0]) == type(col[-1]):
+	#	dataTypes[h] = 
+	#	print 'GreenSeasXLtoNC:\tInfo:\t',h, ncVarName[h], ' is type:',  dataTypes[h]
+	#else:
+	#	print 'GreenSeasXLtoNC:\tWARNING:\tTWO KINDS OF DATA IN', h, ncVarName[h], [col[0],type(col[0]) ],[col[-1], type(col[-1])]
+  	#data type must be one of ['i8', 'f4', 'u8', 'i1', 'u4', 'S1', 'i2', 'u1', 'i4', 'u2', 'f8']
+
   def _saveShelve_(self):
 	print 'Saving output Shelve'
 	if exists(self.outShelveName):
@@ -245,6 +276,9 @@ class GreenSeasXLtoNC:
 	nco = Dataset(self.fno,'w')	
 	nco.setncattr('CreatedDate','This netcdf was created on the '+str(date.today()) +' by '+getuser()+' using GreenSeasXLtoNC.py')
 	nco.setncattr('Original File',self.fni)	
+	for att in self.attributes.keys():
+		print 'GreenSeasXLtoNC:\tInfo:\tAdding Attribute:', att, self.attributes[att]
+		nco.setncattr(att, self.attributes[att])
 	
 	nco.createDimension('i', None)
 	
@@ -270,8 +304,11 @@ class GreenSeasXLtoNC:
 		if v in self.emptyColummns:continue
 		if v in self.oneValueInColumn:continue
 		print 'GreenSeasXLtoNC:\tInfo:\tSaving data:',v,self.ncVarName[v]
-		arr =  array(self.data[v])
-		nco.variables[self.ncVarName[v]][:] = arr
+		arr =  []
+		for a,val in enumerate(self.data[v]):
+			if self.rowcounts[a]== 0:continue
+			arr.append(val)	
+		nco.variables[self.ncVarName[v]][:] = marray(arr)
 				
 	
 
--- runGSXLNC.py
+++ runGSXLNC.py
@@ -5,60 +5,9 @@ fni = 'xlsx/AtlanticData_short.xlsx'
 fno = 'tmp.nc'
 
 
-a = GreenSeasXLtoNC(fni,fno, saveShelve=False,saveNC=False)
-
-
-
-
+a = GreenSeasXLtoNC(fni,fno, saveShelve=False,saveNC=True)
 
 #other stuff
 
 datasheet = a.datasheet
-
-def otherstuff():
-	locator=[]
-	for h in datasheet.row(11)[0:20]:	locator.append(h.value)
-	key={}
-	for n,l in enumerate(locator):
-			if l.lower() in [ 'lat', 'latitude']: key['lat']=n
-			if l.lower() in [ 'lon','long', 'longitude']: key['lon']=n
-			if l in [ 'time','t', 'Date& Time (local)']: key['time']=n
-			if l in [ 'Depth of sample [m]']: key['z']=n
-			if l in [ 'Depth of Sea [m]',]: key['bathy']=n
-			if l in [ 'UTC offset',]: key['tOffset']=n
-			if l in ['Institute',]: key['Institute']=n
-
-	time = datasheet.col(key['time'])[20:]
-
-	header   = [h.value for h in datasheet.row(1)]
-	units    = [h.value for h in datasheet.row(2)]
-	locator  = [h.value for h in datasheet.row(11)[0:20]]
-	metadata = [h.value for h in datasheet.col(10)[0:12]]
-
-	datanames=[u'Temperature',]
-	saveCols=[]
-	for h,head in enumerate(header):
-		for d in datanames: 
-			if head.lower().find(d.lower()) > -1:
-				print 'FOUND:\t',d, 'in ',head
-				saveCols.append(h)
-
-	print saveCols
-	lineTitles = [header[h] for h in saveCols ]
-	unitTitles = [units[h]  for h in saveCols ]
-	count =0
-
-	lat  = [h.value for h in datasheet.col(key['lat'])[20:]]
-	lon  = [h.value for h in datasheet.col(key['lon'])[20:]]
-	time = [h.value for h in datasheet.col(key['time'])[20:]]
-	depth= [h.value for h in datasheet.col(key['z'])[20:]]
-
-
-
-
-
-
-
-#time is a bit odd
-#t = xldate_as_tuple(datasheet.col(key['time'])[20:],a.book.datemode)
 
